3329c816a857e342e63c135cdcce6d862d2fd69b
Satake <satake@objectfoo.com>
2011-08-04 21:54:07 -0700

    Short Code upate
    Expanded shortcode: render feedback pages based on dreamiest redirect.
    Hardcoded redirect urls so the dreamhost server will redirect back to
    the shortcode page so I can render up an appropriate response.  Added
    support for query_vars: dh_response_page, address, name and code.

--- php/ShortCode_SHB.php
+++ php/ShortCode_SHB.php
@@ -2,13 +2,15 @@
 class ShortCode_SHB {
 	static $need_assets;
 	
-	function __construct($admin) {
+	function __construct( $admin ) {
 		$this->admin = $admin;
 	}
 	
 	function init() {
 		add_shortcode( 'announcelist',  array( $this, 'replace_shortcode') );
 		add_action( 'init',             array( $this, 'enqueue_assets' ) );
+		add_filter( 'query_vars',       array( $this, 'add_query_vars' ) );
+
 		
 		// can't dequeue stylesheets cause they don't print in the footer :(
 		// add_action( 'the_post',					array( $this, 'post_has_shortcode' ) );
@@ -17,30 +19,39 @@ class ShortCode_SHB {
 	
 	function replace_shortcode() {
 		// replace the shortcode with the subscribe / unsubscribe form
+		// also show feedback from server when it redirects the browser back to this
+		// page (set in the webpanel)
 		$opts = $this->admin->getOptions();
 		if( $opts[SHB_ANNOUNCE_KEY] ) {
-		?>
-		<div id="subscribe-hoa">
-			<div class="hd">
-				<p><?php echo bloginfo( 'name' ) ?> Announcement List Subscription</p>
-			</div>
-			
-			<form method="post" action="http://scripts.dreamhost.com/add_list.cgi">
-				<input type="hidden" name="list" value="<?php echo $opts[SHB_LIST_KEY] ?>" id="list" />
-				<input type="hidden" name="domain" value="<?php echo $opts[SHB_DOMAIN_KEY] ?>" id="domain" />
-				<table border="0" cellspacing="0" cellpadding="0">
-					<tr>
-						<td class="left"><label for="email">E-mail:</label> <input type="text" name="email" value="" id="email" /></td>
-						<td><input type="submit" name="submit" value="Subscribe" id="submit" /></td>
-						<td><input type="submit" name="unsub" value="Unsubscribe" id="unsub"></td>
-					</tr>
-				</table>
-				
-				
-				
-			</form>
-		</div>
-		<?php
+		    $dh_response_page = get_query_var( 'dh_response_page' );
+		    $response_data = array(
+		      'address' => get_query_var( 'address' ),
+		      'name' => get_query_var( 'name' ),
+		      'code' => get_query_var( 'code' ),
+		      'type' => $dh_response_page
+		    );
+
+            // $address = get_query_var( 'address' );
+            // $name = get_query_var( 'name' );
+            // $code = get_query_var( 'code' );
+
+             if( empty($dh_response_page) ) {
+                 // this is not a response page from dreamhost
+                 $this->render_subcribe_form();
+             } else {
+                $this->render_placeholder_response( $response_data );
+                 // this is a response page from dreamhost
+                 // subscribed: confirmation email has been sent
+                 // already_subscribed: goodbye page
+                 // already_on: you are already on the list
+                 // not_subscribed: you are not on the list
+                 // invalid_email: malformed email address
+                 // welcome: Email confirm (they just clicked the confirm link in their email)
+                 
+                 
+                 
+             }
+
 		}
 	}
 	
@@ -67,5 +78,60 @@ class ShortCode_SHB {
 			wp_dequeue_style( 'subscribe-hoa' );
 		}
 	}
+	
+    function add_query_vars( $vars ) {
+        $vars[] = 'dh_response_page';
+		$vars[] = 'address';
+		$vars[] = 'name';
+		$vars[] = 'code';
+		return $vars;
+    }
+
+    // render subscription form
+    function render_subcribe_form() {
+		$opts = $this->admin->getOptions();
+        ?>
+		<div id="subscribe-hoa">
+			<div class="hd">
+				<p><?php echo bloginfo( 'name' ) ?> Announcement List Subscription</p>
+			</div>
+		
+			<form method="post" action="http://scripts.dreamhost.com/add_list.cgi">
+				<input type="hidden" name="list" value="<?php echo $opts[SHB_LIST_KEY] ?>" id="list" />
+				<input type="hidden" name="domain" value="<?php echo $opts[SHB_DOMAIN_KEY] ?>" id="domain" />
+
+                <input type="hidden" name="url" value="<?php echo get_permalink() .'/?dh_response_page=subscribed' ?>" /> 
+                <input type="hidden" name="unsuburl" value="<?php echo get_permalink() .'/?dh_response_page=unsubscribed' ?>" /> 
+                <input type="hidden" name="alreadyonurl" value="<?php echo get_permalink() .'/?dh_response_page=already_subscribed' ?>" /> 
+                <input type="hidden" name="notonurl" value="<?php echo get_permalink() .'/?dh_response_page=not_subscribed' ?>" /> 
+                <input type="hidden" name="invalidurl" value="<?php echo get_permalink() .'/?dh_response_page=invalid_email' ?>" /> 
+                <?php
+                /*
+                don't think I need this one, it's set via the webpanel
+                <input type="hidden" name="emailconfirmurl" value="<?php echo get_permalink() .'/?dh_response_page=confirm' ?>" />
+                */ ?>
+                
+
+				<table border="0" cellspacing="0" cellpadding="0">
+					<tr>
+						<td class="left"><label for="email">E-mail:</label> <input type="text" name="email" value="" id="email" /></td>
+						<td><input type="submit" name="submit" value="Subscribe" id="submit" /></td>
+						<td><input type="submit" name="unsub" value="Unsubscribe" id="unsub"></td>
+					</tr>
+				</table>
+			</form>
+		</div>
+        <?php
+    }
+    
+    // render placeholder response page
+    function render_placeholder_response( $response_data ) {
+        ?>
+        <div id="subscribe-hoa">
+            <p>Response Page: <?php echo $response_data['type'] ?></p>
+        </div>
+        <?php
+    }
+
 }
 ?>
